741d038585e4a27dc806b73ca9f8282df1cc2721
jianhe2git <59978308+noddyc@users.noreply.github.com>
2023-05-15 01:03:52 -0400

    1

--- query.js
+++ query.js
@@ -2,7 +2,7 @@ const db = require('./db')
 
 
 async function queryAllData(){
-    const sqlStatement = "SELECT UN.fdcid, UN.servingSize, UN.calories, UN.protein, UN.totalFat, UN.sugars, UN.vitaminD, UN.calcium, UN.iron, UN.potassium, UN.fiber, UN.sodium, UN.artificial,UN.houseHoldServing,UN.saturatedFat, UN.sweetener, UN.goodChoice, UN.tag, UI.description, UI.brand, UI.ingredients FROM usdaNutrientUpdated AS UN JOIN usdaInfoUpdated AS UI ON UN.fdcid = UI.fdcid "
+    const sqlStatement = "SELECT UN.fdcid, UN.servingSize, UN.calories, UN.protein, UN.totalFat, UN.sugars, UN.vitaminD, UN.calcium, UN.iron, UN.potassium, UN.fiber, UN.sodium, UN.artificial,UN.houseHoldServing,UN.saturatedFat, UN.sweetener, UN.goodChoice, UN.tag, UI.description, UI.brand, UI.ingredients FROM usdaNutrientUpdated AS UN JOIN usdaInfoUpdated AS UI ON UN.fdcid = UI.fdcid limit 1000"
     const result = await db.query(sqlStatement);
     return result[0];
 }
